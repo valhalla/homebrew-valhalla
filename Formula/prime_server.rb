aa5148d7ecebd967c74a894b83e05074e6870e50
Drew Dara-Abrams <dda@dara-abrams.com>
2018-01-27 14:52:37 -0800

    formula improvements

--- Formula/prime_server.rb
+++ Formula/prime_server.rb
@@ -1,8 +1,7 @@
 class PrimeServer < Formula
-  desc "non-blocking (web)server API for distributed computing and SOA based on zeromq"
+  desc "Non-blocking web server API based on ZeroMQ"
   homepage "https://github.com/kevinkreiser/prime_server"
-  version "0.6.3"
-  url "https://github.com/kevinkreiser/prime_server.git", tag: version
+  url "https://github.com/kevinkreiser/prime_server.git", :tag => "0.6.3"
   sha256 "dd891bc74e2220597460b3811a1bc4e3488aaf809c1fa6bfbc849b564bc8d597"
 
   depends_on "autoconf" => :build
@@ -23,6 +22,6 @@ class PrimeServer < Formula
   end
 
   test do
-    pipe_output("prime_serverd").include?("Usage: prime_serverd num_requests|server_listen_endpoint concurrency")
+    pipe_output("{bin}/prime_serverd").include?("Usage: prime_serverd num_requests|server_listen_endpoint concurrency")
   end
 end
--- Formula/valhalla.rb
+++ Formula/valhalla.rb
@@ -1,31 +1,38 @@
 class Valhalla < Formula
-  desc "Open Source Routing Engine for OpenStreetMap"
+  desc "Routing engine for OpenStreetMap, Transitland, and elevation tiles"
   homepage "https://github.com/valhalla/valhalla/"
-  version "2.4.6"
-  url "https://github.com/valhalla/valhalla.git", tag: version, shallow: true
+  url "https://github.com/valhalla/valhalla.git", :tag => "2.4.6", :shallow => true
   sha256 "e24207d520fb6edaf82e93ce593f12f0f6f1278aef30e1d8ab5893890f474374"
 
-  ["autoconf", "automake", "libtool", "boost-python", "libspatialite", "geos", "pkg-config"].each do |package|
+  option "without-python-bindings", "Skip compiling optional Python bindings"
+
+  ["autoconf", "automake", "libtool", "libspatialite", "geos", "pkg-config"].each do |package|
     depends_on package => :build
   end
-  ["protobuf", "protobuf-c", "sqlite3", "lua", "jq", "curl", "prime_server", "lz4"].each do |package|
+  ["protobuf", "protobuf-c", "sqlite", "lua", "jq", "curl", "prime_server", "lz4"].each do |package|
     depends_on package
+  end
+
+  unless build.without? "python-bindings"
+    depends_on "boost-python" => :build
   end
 
   def install
     system "./autogen.sh"
-    system 'export LDFLAGS="-L/usr/local/opt/sqlite/lib/ -lsqlite3"'
-    system "export PKG_CONFIG_PATH=/usr/local/opt/curl/lib/pkgconfig"
+    system "export LDFLAGS=\"-L#{HOMEBREW_PREFIX}/opt/sqlite/lib/ -lsqlite3\""
+    system "export PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--with-protobuf-libdir=/usr/local/opt/protobuf-c/lib",
-                          "--with-protoc=/usr/local/opt/protobuf/bin/protoc"
+                          "--with-protobuf-libdir=#{HOMEBREW_PREFIX}/opt/protobuf-c/lib",
+                          "--with-protoc=#{HOMEBREW_PREFIX}/opt/protobuf/bin/protoc",
+                          # "--link-python-framework-via-dynamic-lookup", TODO: improve Python module stuff
+                          "--enable-python-bindings=#{build.without?("python-bindings") ? 'no' : 'yes'}"
     system "make", "install"
   end
 
   test do
-    pipe_output("valhalla_service").include?("Usage: valhalla_service config/file.json [concurrency]")
+    pipe_output("${bin}/valhalla_service").include?("Usage: valhalla_service config/file.json [concurrency]")
   end
 end
